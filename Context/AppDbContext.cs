1444d87228299380fd13466cd8d96e968d014534
Alexandra Alcantara <senhorita.alexandra@hotmail.com>
2025-09-23 21:00:04 -0300

    feat: add initial database context and app setup

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+using APICatalog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalog.Context;
+
+public class AppDbContext : DbContext
+{
+	public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+	{
+	}
+
+	public DbSet<Category> Categories { get; set; }
+	
+	public DbSet<Product> Products { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+using APICatalog.Context;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+string? mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+if (string.IsNullOrEmpty(mySqlConnection))
+{
+    throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+}
+
+mySqlConnection = mySqlConnection
+    .Replace("${DB_HOST}", Environment.GetEnvironmentVariable("DB_HOST"))
+    .Replace("${DB_NAME}", Environment.GetEnvironmentVariable("DB_NAME"))
+    .Replace("${DB_USER}", Environment.GetEnvironmentVariable("DB_USER"))
+    .Replace("${DB_PASSWORD}", Environment.GetEnvironmentVariable("DB_PASSWORD"));
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(mySqlConnection, ServerVersion.AutoDetect(mySqlConnection)));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
