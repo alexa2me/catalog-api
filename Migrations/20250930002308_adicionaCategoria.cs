856d78106de52a6b1d374ce04c78b784e99824d8
Alexandra Alcantara <senhorita.alexandra@hotmail.com>
2025-09-29 21:32:10 -0300

    chore: add migration to insert a new category

---
+++ Migrations/20250930002308_adicionaCategoria.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalog.Migrations
+{
+    /// <inheritdoc />
+    public partial class adicionaCategoria : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Categories (Name, ImageUrl) VALUES ('Substantivo', 'https://www.beveragedaily.com/resizer/v2/QSRDJ7WC7RP6XAPDJSS4ZS5F2A.jpg?auth=db930caaa79100ecb2af1a5b95f3b92843cf33846a1df0f391c58a77f79d0b7f')");
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Categories WHERE Name = 'Substantivo'");
+        }
+    }
+}
