3503f0d76940fe6c9e729d2929c83f7e7808971a
Alexandra Alcantara <senhorita.alexandra@hotmail.com>
2025-10-11 15:21:16 -0300

    refactor: add custom validation for capitalized name and inventory

--- Models/Product.cs
+++ Models/Product.cs
@@ -5,20 +5,32 @@ using System.Text.Json.Serialization;
 namespace APICatalog.Models;
 
 [Table("Products")]
-public class Product
+public class Product : IValidatableObject
 {
     [Key]
     public int Id { get; set; }
     [Required]
-    [StringLength(80, MinimumLength = 2, ErrorMessage = "Name must be between 2 and 80 characters")]
+    [StringLength(
+        80,
+        MinimumLength = 2,
+        ErrorMessage = "Name must be between 2 and 80 characters"
+    )]
+    // [FirstLetterCapitalized]
     public string? Name { get; set; }
 
     [Required]
-    [StringLength(300, MinimumLength = 10, ErrorMessage = "Description must be between 10 and 300 characters")]
+    [StringLength(
+        300,
+        MinimumLength = 10,
+        ErrorMessage = "Description must be between 10 and 300 characters"
+    )]
     public string? Description { get; set; }
 
     [Required]
-    [StringLength(300, ErrorMessage = "ImageUrl must have maximum 300 characters")]
+    [StringLength(
+        300,
+        ErrorMessage = "ImageUrl must have maximum 300 characters"
+    )]
     public string? ImageUrl { get; set; }
 
     [Required]
@@ -26,7 +38,11 @@ public class Product
     [Range(1, 10000, ErrorMessage = "Price must be between {1} and {2}")]
     public decimal Price { get; set; }
 
-    [Range(0, float.MaxValue, ErrorMessage = "Inventory must be a positive number or zero")]
+    // [Range(
+    //     0,
+    //     float.MaxValue,
+    //     ErrorMessage = "Inventory must be a positive number or zero"
+    // )]
     public float Inventory { get; set; }
 
     public DateTime RegistrationDate { get; set; }
@@ -35,4 +51,39 @@ public class Product
 
     [JsonIgnore]
     public Category? Category { get; set; }
+
+
+    // Approach using IValidatableObject.
+    // It seems very polluted to create these validations
+    // along with the models.
+    public IEnumerable<ValidationResult> Validate(
+        ValidationContext validationContext
+        )
+    {
+        if (!string.IsNullOrEmpty(Name))
+        {
+            var firstLetter = Name[0].ToString();
+            if (!string.Equals(
+                    firstLetter,
+                    firstLetter.ToUpper(),
+                    StringComparison.Ordinal
+                    )
+                )
+            {
+                yield return new ValidationResult(
+                    "The first letter of the name must be capitalized.",
+                    [nameof(Name)]
+                    );
+            }
+
+            if (Inventory < 0)
+            {
+                yield return new
+                    ValidationResult(
+                        "Inventory must be a positive number or zero.",
+                        [nameof(Inventory)]
+                    );
+            }
+        }
+    }
 }
---
+++ Validations/FirstLetterCapitalizedAttribute.cs
@@ -0,0 +1,29 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace APICatalog.Validations;
+
+public class FirstLetterCapitalizedAttribute : ValidationAttribute
+{
+    protected override ValidationResult IsValid(object? value,
+		ValidationContext validationContext)
+    {
+		if (value == null || string.IsNullOrEmpty(value.ToString()))
+		{
+			return ValidationResult.Success!;
+		}
+
+		var strValue = value?.ToString();
+		if (string.IsNullOrEmpty(strValue))
+		{
+			return ValidationResult.Success!;
+		}
+
+		var firstLetter = strValue[0].ToString();
+		if (firstLetter != firstLetter?.ToUpper())
+		{
+			return new ValidationResult("The first letter of the name must be capitalized.");
+		}
+
+		return ValidationResult.Success!;
+    }
+}
